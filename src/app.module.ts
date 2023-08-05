e4444ea5fa1e389df392753ed7aee5adb85a09d7
AliReza Rouhani <alireza.rou13@gmail.com>
2023-08-05 06:39:30 -0600

    init prisma module and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigsModule } from './configs/configs.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigsModule],
+  imports: [ConfigsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
